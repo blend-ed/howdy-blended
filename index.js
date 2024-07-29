254fa9dae5136f1e7edcae4b4ca3e3e69883225a
Rabeeh T A <rabeehchemnad@gmail.com>
2024-07-29 11:34:14 +0530

    refactor: Update howdy function to include version number

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function howdy() {
-    return "hello from Blend-ed"
+    return "hello from Blend-ed version is 2.0.0"
 }
 
 function howdyLog() {
-    console.log("hello from Blend-ed");
+    console.log("hello from Blend-ed is 2.0.0");
 }
 
 module.exports = { howdy, howdyLog };

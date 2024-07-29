607a51a47d7486c164eb5d3615fc00ae1b2acd92
Rabeeh T A <rabeehchemnad@gmail.com>
2024-07-29 11:35:02 +0530

    Refactor howdy function and update package name

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function howdy() {
-    return "hello from v1 of Blend-ed!"
+    return "hello from v1 of Blend-ed! 2.0.0"
 }
 
 function howdyLog() {
-    console.log("hello from v1 of Blend-ed");
+    console.log("hello from v1 of Blend-ed 2.0.0");
 }
 
 module.exports = { howdy, howdyLog };
